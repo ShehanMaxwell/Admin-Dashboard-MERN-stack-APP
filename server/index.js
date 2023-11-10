439f30052d9e427a1c19ae0fc7aafb79ab726fda
Shehan Maxwell <shehanbbn@gmail.com>
2023-11-10 13:36:32 +0530

    modified index.js and package.json in server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import clientRoutes from './routes/client.js';
-import generalRoutes from './routes/generaljs';
+import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
@@ -26,3 +26,15 @@ app.use('/client', clientRoutes);
 app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9003;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port:${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
