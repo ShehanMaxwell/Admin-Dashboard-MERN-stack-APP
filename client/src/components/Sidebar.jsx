aa0e5369bef68282d11f4b6c39b430cce0358810
Shehan Maxwell <shehanbbn@gmail.com>
2023-11-13 22:22:01 +0530

    modified Sidebar.jsx in components

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,40 +1,88 @@
-import React from 'react'
+import React from 'react';
 import {
-    Box,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItems,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Typography,
-    useTheme,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItems,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
 } from '@mui/material';
 import {
-    SettingsOutlined,
-    ChevronLeft,
-    ChevronRightOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    Groups2Outlined,
-    ReceiptLongOutlined,
-    PublicOutlined,
-    pointOfSaleOutlined,
-    TodayOutlined,
-    CalenderMonthOutlined,
-    AdminPanelSettingOutlined,
-    TrendingUpOutlined,
-    PieChartOutlined
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  pointOfSaleOutlined,
+  TodayOutlined,
+  CalenderMonthOutlined,
+  AdminPanelSettingOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
 } from '@mui/icons-material';
-import { useEffect , useState} from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import FlexBetween from './FlexBetween';
+import profileImage from 'assets/profile.jpg';
 
-const Sidebar = () => {
+const Sidebar = ({
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  const { pathname } = useLocation();
+  const [active, setActive] = useState('');
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Box component='nav'>
+      {isSidebarOpen && (
+        <Drawer
+          open={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+          variant='persistent'
+          anchor='left'
+          sx={{
+            width: drawerWidth,
+            '& .MuiDrawer-paper': {
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.alt,
+              boxSixing: 'border-box',
+              borderWidth: isNonMobile ? 0 : '2px',
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box width='100%'>
+            <Box m='1.5rem 2rem 2rem 3rem'>
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box display='flex' alignItems='center' gap='0.5rem'>
+                  <Typography variant='h4' fontWeight='bold'>
+                    MAXWELL ECOM
+                  </Typography>
+                </Box>
+                
+              </FlexBetween>
+            </Box>
+          </Box>
+        </Drawer>
+      )}
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
