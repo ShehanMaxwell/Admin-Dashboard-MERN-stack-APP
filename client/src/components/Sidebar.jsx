2583bb7ffffa902cf044d8c8651db28f196e6ea9
Shehan Maxwell <shehanbbn@gmail.com>
2023-11-13 18:28:04 +0530

    modified index.jsx added Sidebar.jsx

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItems,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    useTheme,
+} from '@mui/material';
+import {
+    SettingsOutlined,
+    ChevronLeft,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutlined,
+    PublicOutlined,
+    pointOfSaleOutlined,
+    TodayOutlined,
+    CalenderMonthOutlined,
+    AdminPanelSettingOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined
+} from '@mui/icons-material';
+import { useEffect , useState} from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
