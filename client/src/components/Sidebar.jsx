a499819f96cd87d9013fde7af346329dcc9c2ca8
Shehan Maxwell <shehanbbn@gmail.com>
2023-11-15 16:34:45 +0530

    modified Sidebar.jsx in components

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,6 +11,7 @@ import {
   ListItemText,
   Typography,
   useTheme,
+  ListItem,
 } from '@mui/material';
 import {
   SettingsOutlined,
@@ -23,17 +24,75 @@ import {
   PublicOutlined,
   pointOfSaleOutlined,
   TodayOutlined,
-  CalenderMonthOutlined,
-  AdminPanelSettingOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
   TrendingUpOutlined,
   PieChartOutlined,
+  PointOfSaleOutlined,
 } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import FlexBetween from './FlexBetween';
 import profileImage from 'assets/profile.jpg';
 
-
+const navItems = [
+  {
+    text: 'Dashboard',
+    icon: <HomeOutlined />,
+  },
+  {
+    text: 'Client Facing',
+    icon: null,
+  },
+  {
+    text: 'Products',
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: 'Customers',
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: 'Transactions',
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: 'Geography',
+    icon: <PublicOutlined />,
+  },
+  {
+    text: 'Sales',
+    icon: null,
+  },
+  {
+    text: 'Overview',
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: 'Daily',
+    icon: <TodayOutlined />,
+  },
+  {
+    text: 'Monthly',
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: 'Breakdown',
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: 'Management',
+    icon: null,
+  },
+  {
+    text: 'Admin',
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: 'Performance',
+    icon: <TrendingUpOutlined />,
+  },
+];
 
 const Sidebar = ({
   drawerWidth,
@@ -74,18 +133,64 @@ const Sidebar = ({
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display='flex' alignItems='center' gap='0.5rem'>
                   <Typography variant='h4' fontWeight='bold'>
-                    MAXWELL ECOM
+                    MAXWEL E-COM
                   </Typography>
                 </Box>
                 {!isNonMobile && (
-                  <IconButton onClick={()=> setIsSidebarOpen(!isSidebarOpen)}>
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                     <ChevronLeft />
                   </IconButton>
                 )}
               </FlexBetween>
             </Box>
             <List>
-              {}
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: '2.25rem 0 1rem 3rem' }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLowerCase();
+
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText
+                            ? theme.palette.secondary[300]
+                            : 'transparent',
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          ml: '2rem',
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[200],
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: 'auto' }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
             </List>
           </Box>
         </Drawer>
